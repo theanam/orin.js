3a46f7ab8baa1321a263e4f42ff1ab993827645a
Anam Ahmed <anam.ahmed.a@gmail.com>
2014-06-30 01:53:13 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+#jQuery Orin.js
+***********
+A tiny jQuery lightbox plugin with cool effects.
+
+###So, what does it actually do? 
+
+well, it's like an ordinary lightbox, but it can create pretty awesome effects on the background.
+
+
+###how to try
+here's the simple way, documentation coming soon.
+
+Include jQuery and the two files in your document:
+
+	<link rel="stylesheet" href="jquery.orin.css">
+	<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>	
+	<script src="jquery.orin.js" type="text/javascript"></script>
+
+
+for example, let's assume you want lightbox in your `div` elements with class `lbox`
+
+
+sample markup:
+
+	<div class="lbox" data-image="a.jpg" data-text="first image details">
+	Click to open first image
+	</div>
+	<div class="lbox" data-image="b.jpg" data-text="Second image details">
+	 Click to open second image
+	</div>
+
+add these javascript codes just before the closing `</body>` tag,
+
+	<script type="text/javascript" charset="utf-8">
+		$('.lbox').orin({
+			'effect':'blur' //effect name , other options are grayscale,invert,sepia,hue-rotate
+		});
+	</script>
+
+This is a temporary readme, bugs are welcome
+
+ 
+
+
---
+++ jquery.orin.js
@@ -0,0 +1,71 @@
+/* jQuery Orin Lightbox
+ * Copyright : Anam Ahmed
+ * Url : http://anam.co/
+ * License : MPL (Mozilla Public License)
+ * Disclaimer : Webkit only feature, don't expect too much from non webkit browsers, if it 
+ * destroys your house or kill your cat, I'm not responsible.
+ */
+(function($){$.fn.extend({
+	'orin': function(userargs){
+		//console.log(userargs);
+		// set variables or use defaults
+		var effectname   = userargs.effect || 'blur';
+		var effectrate   = userargs.rate || '5px';
+		var effectamount = userargs.amount || 1;
+		//lightbox div, if not available, create it
+		var lightboxdiv = userargs.lightboxdiv || '#orin-wrapper';
+		if($(lightboxdiv).length<1){
+			var divname = lightboxdiv.slice(1,lightboxdiv.length);
+			$('body').append('<div id="'+divname+'"></div>');
+		}
+		// now get the lightbox div element
+		var target = $(lightboxdiv);
+		target.addClass('orinjs-overlay');
+		target.append('<img id="orinjs-canvas" src="">');
+		target.append('<p id ="orinjs-text"><span></span></p>');
+		target.append('<div id="orinjs-closebutton"></div>');
+		var theimage=$('#orinjs-canvas');
+		var thetext=$('#orinjs-text');
+		$('#orinjs-closebutton').click(function(){
+			target.hide(10);
+			theimage.attr('src',"");
+			thetext.find('span').html("");
+			$('body').removeClass('orinjs-active');
+		});
+		switch(effectname){
+					case 'blur':
+					$('body').addClass('orinjs-blur');
+					break;
+					case 'grayscale':
+					$('body').addClass('orinjs-grayscale');
+					break;
+					case 'sepia':
+					$('body').addClass('orinjs-sepia');
+					break;
+					case 'invert':
+					$('body').addClass('orinjs-invert');
+					break;
+					case 'huerotate':
+					$('body').addClass('orinjs-hue-rotate');
+					break;
+					default:
+					$('body').addClass('orinjs-blur'); //blur, if the coder sends some
+				}
+		// make the thing look clickable
+		$(this).css('cursor','pointer');
+		//per element function		
+		$(this).each(function(){
+			$(this).click(function(){
+				console.log(this);
+				var theimagepath=$(this).attr('data-image');
+				console.log(theimagepath);
+				theimage.attr('src',theimagepath);
+				thetext.find('span').html($(this).attr('data-text'));
+				target.show(10);
+				// the effect magic
+				$('body').addClass('orinjs-active');
+			});
+			
+	});
+	}
+});})(jQuery);
